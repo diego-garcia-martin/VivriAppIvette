0c7fdd6bf1d84b71843b3e6a235f5b78c168addd
diego-garcia-martin <garcia.martin.diego@outlook.com>
2016-07-17 21:06:43 -0500

    Links del menu principal ya funcionan

--- app/src/main/java/non/vivriapp/MenuPrincipal.java
+++ app/src/main/java/non/vivriapp/MenuPrincipal.java
@@ -1,17 +1,30 @@
 package non.vivriapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.PopupMenu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MenuPrincipal extends AppCompatActivity {
+    ImageButton testimonios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        testimonios = (ImageButton)findViewById(R.id.btn_testimonios);
+        testimonios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://retovivri.com/historias-vivri-new-englishh/"));
+                startActivity(intent);
+            }
+        });
     }
 
     public void paisCompras(View view){
@@ -19,6 +32,21 @@ public class MenuPrincipal extends AppCompatActivity {
         MenuInflater inflater = popupMenu.getMenuInflater();
         inflater.inflate(R.menu.menu_compras, popupMenu.getMenu());
         popupMenu.show();
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId() == R.id.comprasMexico){
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://it.vivri.com/shop/index.php?SPONSORID=70683&pais=1&display=1#modal1"));
+                    startActivity(intent);
+                }
+                else if(item.getItemId() == R.id.comprasUSA){
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://it.vivri.com/shop/index.php?SPONSORID=70683&pais=2&display=1#modal1"));
+                    startActivity(intent);
+                }
+                return true;
+            }
+        });
     }
 
     public void paisInscribete(View view){
@@ -26,5 +54,20 @@ public class MenuPrincipal extends AppCompatActivity {
         MenuInflater inflater = popupMenu.getMenuInflater();
         inflater.inflate(R.menu.menu_inscribete, popupMenu.getMenu());
         popupMenu.show();
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId() == R.id.inscribeteMexico){
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://it.vivri.com/inscripciones/enrollmentmx.php?DealerID=70683"));
+                    startActivity(intent);
+                }
+                else if(item.getItemId() == R.id.inscribeteUSA){
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://it.vivri.com/inscripciones/enrollmentus.php?DealerID=70683"));
+                    startActivity(intent);
+                }
+                return true;
+            }
+        });
     }
 }
