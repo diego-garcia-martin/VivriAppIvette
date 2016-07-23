2fe57919a3008472e9c75b1f664c567405d27fda
diego-garcia-martin <garcia.martin.diego@outlook.com>
2016-07-23 16:38:28 -0500

    Creadas extras para recetas, hay que terminarlo

---
+++ app/src/main/java/non/vivriapp/Recetas02.java
@@ -0,0 +1,13 @@
+package non.vivriapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Recetas02 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recetas02);
+    }
+}
--- app/src/main/java/non/vivriapp/RecetasActivity.java
+++ app/src/main/java/non/vivriapp/RecetasActivity.java
@@ -1,7 +1,10 @@
 package non.vivriapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RecetasActivity extends AppCompatActivity {
 
@@ -9,5 +12,37 @@ public class RecetasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recetas);
+    }
+    public void abrirEnsaladas(View view){
+        Intent intent = new Intent(this, Recetas02.class);
+        startActivity(intent);
+    }
+    public void abrirPollo(View view){
+        Intent intent = new Intent(this, Recetas02.class);
+        startActivity(intent);
+    }
+    public void abrirRes(View view){
+        Intent intent = new Intent(this, Recetas02.class);
+        startActivity(intent);
+    }
+    public void abrirCerdo(View view){
+        Intent intent = new Intent(this, Recetas02.class);
+        startActivity(intent);
+    }
+    public void abrirPastas(View view){
+        Intent intent = new Intent(this, Recetas02.class);
+        startActivity(intent);
+    }
+    public void abrirPescados(View view){
+        Intent intent = new Intent(this, Recetas02.class);
+        startActivity(intent);
+    }
+    public void abrirBocadillos(View view){
+        Intent intent = new Intent(this, Recetas02.class);
+        startActivity(intent);
+    }
+    public void abrirFace(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/vivri.Ivettpaola/?pnref=story"));
+        startActivity(intent);
     }
 }
